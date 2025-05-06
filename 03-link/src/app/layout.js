b3a67f9f88faf6f5cf974531f8f255b24608285a
adimerpaul <adimer101@gmail.com>
2025-05-06 05:32:28 -0400

    Actulizado clase 2

--- 03-link/src/app/layout.js
+++ 03-link/src/app/layout.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function RootLayout({ children }) {
     return (
         <html lang="es">
@@ -5,13 +7,13 @@ export default function RootLayout({ children }) {
             <nav>
                 <ul className="flex gap-2">
                     <li>
-                        <a href="/">Inicio</a>
+                        <Link href="/">Inicio</Link>
                     </li>
                     <li>
-                        <a href="/users">Usuarios</a>
+                        <Link href="/users">Usuarios</Link>
                     </li>
                     <li>
-                        <a href="/about">Acerca de</a>
+                        <Link href="/about">Acerca de</Link>
                     </li>
                 </ul>
             </nav>
--- 03-link/src/app/page.js
+++ 03-link/src/app/page.js
@@ -1,8 +1,8 @@
 export default function Page() {
     return (
         <div>
-            <h1 className="text-3xl font-bold underline">Hola Mundo</h1>
-            <p className="text-gray-700">Este es mi primer proyecto con Next.js</p>
+            <h1>Hola Mundo</h1>
+            <p>Este es mi primer proyecto con Next.js</p>
         </div>
     )
 }
